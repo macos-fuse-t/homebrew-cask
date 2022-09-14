961d6f62805df8e2bb58eed93432f6d37278fbe6
Alex Fishman <alex@veertu.com>
2022-09-14 20:15:27 +0300

    fuse-t cask
    Signed-off-by: Alex Fishman <alex@veertu.com>

---
+++ Casks/fuse-t.rb
@@ -0,0 +1,20 @@
+cask "fuse-t" do
+  version "1.0.1"
+  sha256 "70165f8fcdb20df10f463e3e76d7098d6814a1a020c4f3147969dfbaaa5cf513"
+
+  url "https://github.com/macos-fuse-t/fuse-t/releases/download/#{version}/fuse-t-macos-installer-#{version}.pkg"
+  name "fuse-t"
+  desc "LibFUSE implementation that doesn't use kernel extensions"
+  homepage "https://github.com/macos-fuse-t/fuse-t"
+
+  pkg "fuse-t-macos-installer-#{version}.pkg"
+
+  uninstall script: {
+    executable: "/Library/Application Support/fuse-t/uninstall.sh",
+    sudo:       true,
+  }
+
+  caveats do
+    license "https://github.com/macos-fuse-t/fuse-t/blob/main/License.txt"
+  end
+end
