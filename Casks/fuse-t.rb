b5c2d2179885a86de09cd5b1bf412b1bec8efb5e
Alex Fishman <alex@veertu.com>
2023-03-17 17:19:42 +0200

    Update version
    Signed-off-by: Alex Fishman <alex@veertu.com>

--- Casks/fuse-t.rb
+++ Casks/fuse-t.rb
@@ -1,6 +1,6 @@
 cask "fuse-t" do
-  version "1.0.16"
-  sha256 "bc34c5bc33454db586f8ee5368541b12a64048dfb698aaf6c26bf5a24ed441d6"
+  version "1.0.17"
+  sha256 "a01faa09031f6f016d91a35e664c0126d8f43b06015d2baef44bf3c269d17c56"
 
   url "https://github.com/macos-fuse-t/fuse-t/releases/download/#{version}/fuse-t-macos-installer-#{version}.pkg"
   name "fuse-t"
