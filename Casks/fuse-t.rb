9d88d11bbf76b683e5baafde1edcdfa29ed5041b
Alex Fishman <alex@veertu.com>
2023-07-01 15:29:38 +0300

    fix wrong hash
    Signed-off-by: Alex Fishman <alex@veertu.com>

--- Casks/fuse-t.rb
+++ Casks/fuse-t.rb
@@ -1,6 +1,6 @@
 cask "fuse-t" do
   version "1.0.22"
-  sha256 "7c64a32e00610fb9d01e31b54a341d0bbb8b16c16a488c6810a6bdc8d718d820"
+  sha256 "539430f8b1577daeb12a9f201059619a11b65e8b85becb77582c5a8d55905b6d"
 
   url "https://github.com/macos-fuse-t/fuse-t/releases/download/#{version}/fuse-t-macos-installer-#{version}.pkg"
   name "fuse-t"
