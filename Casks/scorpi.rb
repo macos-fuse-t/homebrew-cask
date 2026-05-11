f5ae39880120fedb310668258c2f7a2c1fd69643
Alex Fishman <alex@fuse-t.org>
2026-05-11 12:10:37 +0300

    Use fixed Scorpi installer package

--- Casks/scorpi.rb
+++ Casks/scorpi.rb
@@ -1,25 +1,15 @@
 cask "scorpi" do
   version "1.0.1"
-  sha256 "e3a63a67d2e248c1849254556018ff158ef5762499cc8a63c5d46669f413caa8"
+  sha256 "3e46e1c2049691f9d71093b9cdc70c339bfc3c1f41d8ff16485504936d3f1208"
 
   url "https://github.com/macos-fuse-t/scorpi/releases/download/#{version}/scorpi-macos-installer-#{version}.pkg"
   name "Scorpi"
   desc "Platform for reproducible Linux and Windows VM environments"
   homepage "https://fuse-t.org/scorpi"
 
-  preflight do
-    expanded_pkg = staged_path/"expanded"
+  pkg "scorpi-macos-installer-#{version}.pkg"
 
-    FileUtils.rm_rf expanded_pkg
-    system_command "/usr/sbin/pkgutil",
-                   args: ["--expand-full", staged_path/"scorpi-macos-installer-#{version}.pkg", expanded_pkg]
-    FileUtils.cp_r expanded_pkg/"scorpi-app.pkg/Payload/Scorpi.app", staged_path/"Scorpi.app"
-    FileUtils.rm_rf expanded_pkg
-    FileUtils.rm_f staged_path/"scorpi-macos-installer-#{version}.pkg"
-  end
-
-  app "Scorpi.app"
-  binary "#{appdir}/Scorpi.app/Contents/Resources/bin/scorpi"
-
-  uninstall quit: "scorpi.fuse-t.org"
+  uninstall quit:    "scorpi.fuse-t.org",
+            delete:  "/usr/local/bin/scorpi",
+            pkgutil: "scorpi.fuse-t.org.app"
 end
